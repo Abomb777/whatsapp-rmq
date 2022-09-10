a310da71be707b508a3991838aeee49660bbed2d
Anton Turbin <abomb82@gmail.com>
2022-09-10 12:53:14 +0300

    patch(env): env normalization

--- app/config.js
+++ app/config.js
@@ -1,6 +1,15 @@
+const yargs = require('yargs/yargs');
+const { hideBin } = require('yargs/helpers');
+const projectVersion = require('./package.json');
+
+const argv = yargs(hideBin(process.argv)).argv;
+console.log(argv);
+for (const k in argv) {
+	console.log(k);
+	if(k!=="_") process.env[k] = argv[k];
+}
+
 module.exports={
 	rmq: 'amqp://user1:user1@rabbitmq',
-//	name: '972528322975',
-	name: '972522033312',
-	version: '1.0.1'
-}
+	version: projectVersion.version
+};
